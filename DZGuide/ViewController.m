78156b650c86d617ff2faf6724b3b68ed12be918
董振宁 <dozening@dozeningdeMacBook-Pro.local>
2018-01-19 10:40:58 +0800

    增加动画效果

--- DZGuide/ViewController.m
+++ DZGuide/ViewController.m
@@ -12,6 +12,8 @@
 
 @interface ViewController (){
     UIImageView* _iv;
+    UIImageView* _switchiv;
+    UIView* _superView;
 }
 
 @end
@@ -22,6 +24,8 @@
     [super viewDidLoad];
     
     [self setupView];
+    [self addMonsoryAnimate];
+    
 }
 - (void)setupView{
     self.view.backgroundColor = [UIColor lightTextColor];
@@ -34,17 +38,19 @@
     }];
     
     UIView* superView = [[UIView alloc]init];
+    _superView = superView;
     superView.frame = self.view.frame;
     [self.view addSubview:superView];
     
     UIImageView* switchiv = [[UIImageView alloc]init];
+    _switchiv = switchiv;
     switchiv.userInteractionEnabled = YES;
     switchiv.image = [UIImage imageNamed:@"Shape"];
     [superView addSubview:switchiv];
     [switchiv mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.right.mas_equalTo(-10);
+        make.right.mas_equalTo(-0);
         make.centerY.mas_equalTo(0);
-        make.size.mas_equalTo(CGSizeMake(40, 40));
+        make.size.mas_equalTo(CGSizeMake(20, 20));
     }];
     UITapGestureRecognizer* tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(click)];
     [switchiv addGestureRecognizer:tap];
@@ -60,7 +66,35 @@
     
     [superView bringSubviewToFront:switchiv];
 }
+- (void)addMonsoryAnimate{
+    
+    [_superView layoutIfNeeded];//如果其约束还没有生成的时候需要动画的话，就请先强制刷新后才写动画，否则所有没生成的约束会直接跑动画
+
+    [UIView animateWithDuration:0.6 animations:^{
+        [_switchiv mas_updateConstraints:^(MASConstraintMaker *make) {
+            
+            make.right.mas_equalTo(-10);
+            
+        }];
+        
+        [_superView layoutIfNeeded];//强制绘制
+    } completion:^(BOOL finished) {
+        //创建动画
+        CAKeyframeAnimation * keyAnimaion = [CAKeyframeAnimation animation];
+        keyAnimaion.keyPath = @"transform.rotation";
+        keyAnimaion.values = @[@(-10 / 180.0 * M_PI),@(10 /180.0 * M_PI),@(-10/ 180.0 * M_PI)];//度数转弧度
+        
+        keyAnimaion.removedOnCompletion = NO;
+        keyAnimaion.fillMode = kCAFillModeForwards;
+        keyAnimaion.duration = 0.3;
+        keyAnimaion.repeatCount = MAXFLOAT;
+        [_switchiv.layer addAnimation:keyAnimaion forKey:nil];
+        
+
+    }];
+}
 - (void)click{
+    [_switchiv.layer removeAllAnimations];
     _iv.hidden = YES;
 }
 - (void)didReceiveMemoryWarning {
